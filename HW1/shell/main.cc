1282de41dda7156f80228951f35933f728d16f05
Aobo Yang <yangaobo@gmail.com>
2019-01-28 17:27:47 -0500

    allow multiple cmds

--- HW1/shell/main.cc
+++ HW1/shell/main.cc
@@ -14,6 +14,8 @@ struct CommandObj {
     std::vector<std::string> tokens;
     std::string redirect_in;
     std::string redirect_out;
+    int pipe_to = 0;
+    int pipe_from = 0;
 };
 
 void parse_and_run_command(const std::string &command) {
@@ -23,11 +25,15 @@ void parse_and_run_command(const std::string &command) {
     std::string format_cmd = command;
     std::istringstream ss (format_cmd);
 
-    CommandObj cmd;
+    std::vector<CommandObj> cmds {CommandObj ()};
+    // CommandObj cmd;
+
     std::string token;
     std::set<std::string> special_chars ({">", "<", "|"});
 
     while (ss >> token) {
+        CommandObj& cmd = cmds.back();
+
         if (special_chars.count(token)) {
             std::string target;
 
@@ -37,8 +43,11 @@ void parse_and_run_command(const std::string &command) {
                 } else if (token == ">") {
                     cmd.redirect_out = target;
                 } else {
-                    std::cerr << "Not implemented.\n";
-                    return;
+                    cmd.pipe_to = 1;
+                    CommandObj new_cmd;
+                    new_cmd.tokens.push_back(target);
+                    new_cmd.pipe_from = 1;
+                    cmds.push_back(new_cmd);
                 }
             } else {
                 std::cerr << "Invalid command\n";
@@ -49,47 +58,53 @@ void parse_and_run_command(const std::string &command) {
         }
     }
 
-    // std::cout << "received tokens (" << cmd.tokens.size() << "):";
-    // for (auto i = cmd.tokens.begin(); i != cmd.tokens.end(); i++) {
-    //     std::cout << *i << ", ";
-    // }
-    // std::cout << '\n';
+    std::cout << "received cmds (" << cmds.size() << "):";
 
-    if (command == "exit") {
-        exit(0);
+    for (auto i = cmds.begin(); i != cmds.end(); i++) {
+        std::cout << i->tokens[0] << ", ";
+    }
+    std::cout << '\n';
+
+    for (auto cmdPtr = cmds.begin(); cmdPtr != cmds.end(); cmdPtr++) {
+        CommandObj cmd = *cmdPtr;
+
+        if (cmd.tokens[0] == "exit") {
+            exit(0);
+        }
+
+        pid_t pid = fork();
+
+        if (pid < 0) {
+            std::cerr << "fork command error\n";
+            exit(1);
+        } else if (pid == 0) {
+            std::vector<char*> charTokens;
+            for (auto i = cmd.tokens.begin(); i != cmd.tokens.end(); i++) {
+                char* str = &(*i)[0];
+                charTokens.push_back(str);
+            }
+            charTokens.push_back(NULL);
+
+            if (cmd.redirect_in != "") {
+                int in_f = open(cmd.redirect_in.c_str(), O_RDONLY);
+                // std::cout << "redirect in: " << cmd.redirect_in << " " << in_f << "\n";
+                dup2(in_f, STDIN_FILENO);
+            }
+
+            if (cmd.redirect_out != "") {
+                int out_f = open(cmd.redirect_out.c_str(), O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+                // std::cout << "redirect out: " << cmd.redirect_out << " " << out_f << "\n";
+                dup2(out_f, STDOUT_FILENO);
+            }
+
+            execvp(charTokens[0], charTokens.data());
+        } else {
+            int status;
+            wait(&status);
+            std::cout << "> Exit status: " << status << "\n";
+        }
     }
 
-    pid_t pid = fork();
-
-    if (pid < 0) {
-        std::cerr << "fork command error\n";
-        exit(1);
-    } else if (pid == 0) {
-        std::vector<char*> charTokens;
-        for (auto i = cmd.tokens.begin(); i != cmd.tokens.end(); i++) {
-            char* str = &(*i)[0];
-            charTokens.push_back(str);
-        }
-        charTokens.push_back(NULL);
-
-        if (cmd.redirect_in != "") {
-            int in_f = open(cmd.redirect_in.c_str(), O_RDONLY);
-            // std::cout << "redirect in: " << cmd.redirect_in << " " << in_f << "\n";
-            dup2(in_f, STDIN_FILENO);
-        }
-
-        if (cmd.redirect_out != "") {
-            int out_f = open(cmd.redirect_out.c_str(), O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-            // std::cout << "redirect out: " << cmd.redirect_out << " " << out_f << "\n";
-            dup2(out_f, STDOUT_FILENO);
-        }
-
-        execvp(charTokens[0], charTokens.data());
-    } else {
-        int status;
-        wait(&status);
-        std::cout << "> Exit status: " << status << "\n";
-    }
 
     // std::cerr << "Not implemented.\n";
 }
