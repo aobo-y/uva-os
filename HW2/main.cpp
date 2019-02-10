e98c214eeffd7afc1861bdefdbf638898643ee1f
Aobo Yang <yangaobo@gmail.com>
2019-02-10 00:59:21 -0500

    HW2: read number

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -1,3 +1,30 @@
-int main(void) {
+#include <string>
+// #include <cstdlib>
+#include <iostream>
+#include <vector>
 
+
+int main(void) {
+    std::string input;
+    std::vector<float> numbers;
+
+    while (true) {
+        std::getline(std::cin, input);
+
+        if (input == "") {
+            break;
+        }
+
+        try {
+            float num = std::stof(input);
+            numbers.push_back(num);
+        } catch (std::exception& e) {
+            std::cerr << "invalid input; exception: " << e.what() << '\n';
+            continue;
+        }
+    }
+
+    // for (auto f = numbers.begin(); f != numbers.end(); f++) {
+    //     std::cout << *f << " ";
+    // }
 }
