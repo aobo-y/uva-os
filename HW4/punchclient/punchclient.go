f3d8b64d1b99bb399f5a9adbecff45cbf78e4209
Aobo Yang <yangaobo@gmail.com>
2019-04-21 00:39:36 -0400

    pipe punch client

--- HW4/punchclient/punchclient.go
+++ HW4/punchclient/punchclient.go
@@ -5,7 +5,47 @@ import (
 	"log"
 	"net"
 	"os"
+	"punch"
+	"strings"
 )
+
+var port string
+
+func pipe(svrconn net.Conn, lclconn net.Conn) {
+	fmt.Println("Pipe " + svrconn.RemoteAddr().String() + " <- " + svrconn.LocalAddr().String() + " <-->" + lclconn.LocalAddr().String() + " -> " + lclconn.RemoteAddr().String())
+
+	punch.Pipe(svrconn, lclconn, nil, nil)
+
+	svrconn.Close()
+	lclconn.Close()
+
+	fmt.Println("Close " + svrconn.RemoteAddr().String() + " <- " + svrconn.LocalAddr().String() + " <-->" + lclconn.LocalAddr().String() + " -> " + lclconn.RemoteAddr().String())
+}
+
+func connect(svrIp string, svrPort string, nounce string) {
+	conn, err := net.Dial("tcp", svrIp+":"+svrPort)
+	if err != nil {
+		log.Fatal("Spin conn to punch server error: ", err)
+		return
+	}
+
+	defer conn.Close()
+	fmt.Println(conn.LocalAddr().String() + " -> " + conn.RemoteAddr().String())
+
+	_, err = conn.Write([]byte(nounce))
+	if err != nil {
+		log.Fatal("Write nounce to punch server error: ", err)
+		return
+	}
+
+	lclconn, err := net.Dial("tcp", ":"+port)
+	if err != nil {
+		log.Fatal("Spin conn to punch server error: ", err)
+		return
+	}
+
+	go pipe(conn, lclconn)
+}
 
 func main() {
 	if len(os.Args) < 5 {
@@ -13,32 +53,34 @@ func main() {
 		return
 	}
 
-	port := os.Args[1]
+	port = os.Args[1]
 	ps := os.Args[2]
 	username := os.Args[3]
 	password := os.Args[4]
 
-	conn, err := net.Dial("tcp", ps)
+	ctrconn, err := net.Dial("tcp", ps)
 
 	if err != nil {
-		log.Fatal("Dial punch server "+ps+" error:", err)
+		log.Fatal("Dial punch server error: ", err)
 	}
 
-	fmt.Println("Connected to punch server:" + ps)
+	fmt.Println("Connected to punch server: " + ps)
 
 	opencmd := "OPEN " + username + " " + password + " " + port
 
-	_, err = conn.Write([]byte(opencmd))
+	_, err = ctrconn.Write([]byte(opencmd))
 	if err != nil {
-		log.Fatal("write error:", err)
+		log.Fatal("write error: ", err)
 		return
 	}
 
+	srvIp, _, _ := net.SplitHostPort(ps)
+
 	for {
 		buf := make([]byte, 1024)
-		n, err := conn.Read(buf[:])
+		n, err := ctrconn.Read(buf[:])
 		if err != nil {
-			log.Fatal("read error:", err)
+			log.Fatal("read error: ", err)
 			return
 		}
 
@@ -47,6 +89,11 @@ func main() {
 		if msg == "FAIL" {
 			log.Fatal("Failed to open port in punch server")
 			return
+		}
+
+		tokens := strings.Split(msg, " ")
+		if len(tokens) == 3 && tokens[0] == "ctrCONNECT" {
+			go connect(srvIp, tokens[1], tokens[2])
 		}
 	}
 }
